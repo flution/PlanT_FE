6582934afd3f6b3510ff8106c60d83da62fea557
elk <flution221@gmail.com>
2024-06-26 15:09:21 +0900

    "feat : Searchbar 수정중"

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SearchBar: React.FC = () => {
   return (
-    <form className="max-w-md mx-auto">
+    <form className="w-11/12 mx-auto">
       <label
         htmlFor="default-search"
         className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
@@ -10,7 +10,7 @@ const SearchBar: React.FC = () => {
         Search
       </label>
       <div className="relative">
-        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
           <svg
             className="w-4 h-4 text-gray-500 dark:text-gray-400"
             aria-hidden="true"
@@ -30,13 +30,13 @@ const SearchBar: React.FC = () => {
         <input
           type="search"
           id="default-search"
-          className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-slate-100 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="가고 싶은 여행을 입력!"
           required
         />
         <button
           type="submit"
-          className="text-white absolute end-2.5 bottom-2.5 bg-main-color hover:bg-hover-color focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-main-color dark:hover:bg-hover-color dark:focus:ring-blue-800"
+          className="text-white absolute right-2.5 bottom-2.5 bg-main-color hover:bg-hover-color focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-main-color dark:hover:bg-hover-color dark:focus:ring-blue-800"
         >
           검색하기
         </button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "node_modules/flowbite/**/*.{js,jsx,ts,tsx}",],
   theme: {
     extend: {
       colors: {
@@ -9,8 +9,18 @@ module.exports = {
         "gray-light": "#F9F9F9",
         "gray-middle": "#E0E0E0",
         "gray-dark": "#C9C9C9",
+        "main-color": "#BFE647",
+        "hover-color": "#89C73E",
+        "gray-light": "#E0E0E0",
+        "focus-color" : "#89C73E",
+      fontSize: {
+        bigBtn: ["20px", "23px"],
+        FltBtn: ["15px", "12px"],
+        midBtn: ["15px", "17px"],
+        smlBtn: ["10px", "11px"],
+        alert: ["15px", "30px"],
       },
     },
   },
-  plugins: [],
+  plugins: ["flowbite/plugin"],
 };
