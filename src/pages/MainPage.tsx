e0197aca460b49e095f3e2ca369cf8f58503391e
jiiminhong <chunsim015@gmail.com>
2024-06-26 12:03:33 +0900

    feat: 가로형 카드 컴포넌트

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+
 import VerticalCard from '../components/VerticalCard';
+import HorizontalCard from '../components/HorizontalCard';
 
 const MainPage: React.FC = () => {
   return (
     <div>
       <h1>
+        <HorizontalCard />
         <VerticalCard />
       </h1>
     </div>
