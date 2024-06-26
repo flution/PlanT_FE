6d18e996cb0143677d187f910d8c3a219ec2891a
Taehoon Kim <thk8lue@gmail.com>
2024-06-26 15:53:50 +0900

    Feat : 버튼알람청유문

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { Routes, Route } from 'react-router-dom';
 import UpperNavbar from './components/UpperNavbar';
 import DetailPage from './pages/DetailPage';
 import MainPage from './pages/MainPage';
-import Alert from "./components/Alert/Alert";
-import ModalRequest from "./components/ModalRequest/ModalRequest";
+import Alert from './components/Alert/Alert';
+import ModalRequest from './components/ModalRequest/ModalRequest';
 
 const App: React.FC = () => {
   return (
@@ -17,7 +17,7 @@ const App: React.FC = () => {
       <Alert text="ID와 일치하지 않습니다." />
       <ModalRequest
         isOpen={true}
-        text={"사용자 정보 활용에 동의하십니까?"}
+        text={'사용자 정보 활용에 동의하십니?'}
       ></ModalRequest>
     </div>
   );
