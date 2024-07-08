596b31b0e3b480742d03734cdc73a037b520a302
elk <flution221@gmail.com>
2024-07-08 14:18:56 +0900

    Temporary commit to save work

---
+++ src/components/LandingPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LCarousel from './Landing/LCarousel';
+
+const LandingPage: React.FC = () => {
+  return (
+    <div>
+      <LCarousel></LCarousel>
+    </div>
+  );
+};
+
+export default LandingPage;
