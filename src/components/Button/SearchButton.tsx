95410cc0ff4482016af5a05a94ca6c24b2f1e7c7
elk <flution221@gmail.com>
2024-07-08 14:58:22 +0900

    "Feat : 검색 바 만들음, API 로직은 아직 안들어가있음"

---
+++ src/components/Button/SearchButton.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+interface SearchButtonProps {
+  onClick: (e: React.FormEvent) => void;
+  isFocused: boolean;
+}
+
+const SearchButton: React.FC<SearchButtonProps> = ({ onClick, isFocused }) => {
+  return (
+    <button
+      type="submit"
+      onClick={onClick}
+      className={`w-10 h-10 rounded transition-colors ${
+        isFocused ? 'bg-main-green' : 'bg-gray-500'
+      }`}
+      style={{
+        backgroundImage: 'url(/img/exportW.png)',
+        backgroundSize: 'contain',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+      }}
+    >
+      <span className="sr-only">Search</span>
+    </button>
+  );
+};
+
+export default SearchButton;
