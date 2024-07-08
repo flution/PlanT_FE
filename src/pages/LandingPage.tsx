d48b55d22f1650b6824f9b5a6fd004e8d259b407
elk <flution221@gmail.com>
2024-07-08 14:38:21 +0900

    "Fix : 망한 브랜치 수정.."

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,22 +1,10 @@
-import React, { useState } from 'react';
-import LCarousel from '../components/Landing/LCarousel';
-import Button from '../components/Button/CarouselButton';
-import { CarouselInterface } from 'flowbite';
+import React from 'react';
+import LCarousel from '../components/LCarousel';
 
 const LandingPage: React.FC = () => {
-  const [carouselInstance, setCarouselInstance] =
-    useState<CarouselInterface | null>(null);
-
   return (
-    <div className="flex flex-col items-center">
-      <div className="w-full h-[160px] bg-white"></div>
-      <LCarousel onCarouselInstance={setCarouselInstance} />
-      <div className="w-full h-[100px] bg-white"></div>
-      <Button
-        text="시작하기"
-        carouselInstance={carouselInstance}
-        lastIndex={2}
-      />
+    <div>
+      <LCarousel></LCarousel>
     </div>
   );
 };
