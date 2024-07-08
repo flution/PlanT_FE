c9366567d213010f6d14aeb949b43bb53ddaafaa
Taehoon Kim <thk8lue@gmail.com>
2024-07-08 15:57:42 +0900

    PLANT-73--MyPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 
 import UpperNavbar from './components/Navbar/UpperNavbar';
 import BottomNavbar from './components/Navbar/BottomNavbar';
@@ -8,16 +8,20 @@ import DetailPage from './pages/DetailPage';
 import MainPage from './pages/MainPage';
 import LoginPage from './pages/Login/LoginPage';
 import AuthSuccessPage from './pages/Login/AuthSuccess';
+import MyPage from './pages/MyPage';
 
 const App: React.FC = () => {
+  const location = useLocation();
+  const hideNavbarPaths = ['/mypage'];
   return (
     <div>
-      <UpperNavbar />
+      {!hideNavbarPaths.includes(location.pathname) && <UpperNavbar />}
       <Routes>
         <Route path="/main" element={<MainPage />} />
         <Route path="/detail" element={<DetailPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/auth/success" element={<AuthSuccessPage />} />
+        <Route path="/mypage" element={<MyPage />} />
       </Routes>
       <BottomNavbar />
     </div>
