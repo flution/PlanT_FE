c1204d66ffe29d211948ad41aa64e8c772da2c1c
Taehoon Kim <thk8lue@gmail.com>
2024-07-11 11:23:46 +0900

    Feat: SearchPage 무한 스크롤 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import UpperNavbar from './components/Navbar/UpperNavbar';
 import BottomNavbar from './components/Navbar/BottomNavbar';
@@ -9,19 +9,20 @@ import MainPage from './pages/MainPage';
 import LoginPage from './pages/Login/LoginPage';
 import AuthSuccessPage from './pages/Login/AuthSuccess';
 import MyPage from './pages/MyPage';
+import SearchPage from './pages/SearchPage';
 
 const App: React.FC = () => {
-  const location = useLocation();
-  const hideNavbarPaths = ['/mypage'];
   return (
     <div>
-      {!hideNavbarPaths.includes(location.pathname) && <UpperNavbar />}
+      {/* {!hideNavbarPaths.includes(location.pathname) && } */}
+      <UpperNavbar />
       <Routes>
         <Route path="/main" element={<MainPage />} />
         <Route path="/detail" element={<DetailPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/auth/success" element={<AuthSuccessPage />} />
         <Route path="/mypage" element={<MyPage />} />
+        <Route path="/search" element={<SearchPage />} />
       </Routes>
       <BottomNavbar />
     </div>
