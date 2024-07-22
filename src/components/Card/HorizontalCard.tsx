de2e19dfc416fa22bc5ca4f680b36c20d6c1845e
Taehoon Kim <thk8lue@gmail.com>
2024-07-22 10:50:28 +0900

    Fix: SearchBar, HorizontalCard

--- src/components/Card/HorizontalCard.tsx
+++ src/components/Card/HorizontalCard.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const HorizontalCard: React.FC = () => {
+interface HorizontalCardProps {
+  title: string;
+  content: string;
+  imageUrl: string;
+}
+
+const HorizontalCard: React.FC<HorizontalCardProps> = ({
+  title,
+  content,
+  imageUrl,
+}) => {
   const navigate = useNavigate();
 
   const handleMoreClick = () => {
@@ -10,30 +20,28 @@ const HorizontalCard: React.FC = () => {
 
   return (
     <div
-      className="flex w-[300px] h-[200px] bg-white border border-gray-200 rounded-lg shadow"
+      className="flex w-80 h-52 bg-white border border-gray-200 rounded-lg shadow"
       onClick={handleMoreClick}
     >
       <a
         href="#"
-        className="flex bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100"
+        className="flex w-80 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100"
       >
         {/* card img */}
         <img
           className="object-cover w-[100px] h-full rounded-l-lg"
-          src="/img/eximgH.png"
+          src={imageUrl}
           alt=""
         />
         {/* card contents */}
         <div className="flex flex-col px-8 py-12 leading-normal">
           {/* card title */}
           <h5 className="mb-2.5 text-[15px] font-['Nexon-Bold'] tracking-tight text-gray-900">
-            제목 텍스트 입니다.
+            {title}
           </h5>
           {/* content */}
           <p className="text-[8px] font-['Nexon-Bold'] text-gray-dark  dark:text-gray-400">
-            내용 텍스트를 첨부하고 싶은 만큼 첨부해야합니다. 장평은 4로 조절하며
-            자간은 따로 없습니다. 웬만하면 회색조의 글로 쓸 수 있게 만들어
-            줍시다.
+            {content}
           </p>
         </div>
       </a>
