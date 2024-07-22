a7cde359b9e575bf7149ddf2ae74718245cd7a51
jiiminhong <chunsim015@gmail.com>
2024-07-22 11:48:58 +0900

    feat: 로그인 버튼 크기 조정, 이미지 추가

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import ButtonBig from '../../components/Button/ButtonBig';
+import ButtonMid from '../../components/Button/ButtonMid';
 
 const LoginPage: React.FC = () => {
   const navigate = useNavigate();
@@ -34,17 +34,23 @@ const LoginPage: React.FC = () => {
 
   return (
     <div>
-      <div className="flex justify-around mx-16 mt-96">
+      <img
+        src="/img/landingLogo.png"
+        className="mx-auto mt-20 mb-5 w-72 h-72"
+        alt="logo"
+        // style={{ marginRight: '10px', marginTop: '10px' }}
+      />
+      <div className="flex justify-around mx-16">
         <img src="/img/LoginBtnNaver.svg" onClick={handleNaverLogin}></img>
         <img src="/img/LoginBtnKakao.svg" onClick={handleKakaoLogin}></img>
         <img src="/img/LoginBtnGoogle.svg" onClick={handleGoogleLogin}></img>
       </div>
       <div>
-        <div className="flex justify-center mt-4" onClick={handleLogin}>
-          <ButtonBig text="이메일로 시작하기" bgColor="main-green" />
+        <div className="flex justify-center mt-7" onClick={handleLogin}>
+          <ButtonMid text="이메일로 시작하기" bgColor="main-green" />
         </div>
         <div className="mt-2.5 flex justify-center" onClick={handleRegister}>
-          <ButtonBig text="회원가입" bgColor="white" />
+          <ButtonMid text="회원가입" bgColor="white" />
         </div>
       </div>
       <div className="flex justify-center mt-3">
